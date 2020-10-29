4a5202c8e8c004adc6d2e2e77430cb18104d080e
Eric Kirimi <ekirimi@innova.co.ke>
2020-10-29 14:31:42 +0300

    attribute selector fix

--- tests/login-model.js
+++ tests/login-model.js
@@ -6,16 +6,16 @@ class Login {
     constructor () {
 
         this.submitBtn = Selector('button').withAttribute('type', 'submit');
-        this.userName = Selector('input').withAttribute('name', 'username'); 
-        this.password = Selector('input').withAttribute('name', 'password'); 
+        this.userName = Selector(()=> document.getElementsByName('username')[0]); 
+        this.password = Selector(()=> document.getElementsByName('password')[0]); 
 
     }
 
     async login () {
 
         this.submitBtn = Selector('button').withAttribute('type', 'submit');
-        this.userName = Selector('input').withAttribute('name', 'username'); 
-        this.password = Selector('input').withAttribute('name', 'password'); 
+        this.userName = Selector(()=> document.getElementsByName('username')[0]); 
+        this.password = Selector(()=> document.getElementsByName('password')[0]); 
         await t
                .maximizeWindow()
                .wait(2000)
